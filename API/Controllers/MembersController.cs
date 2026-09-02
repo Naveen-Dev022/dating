7ab918a1791a75b55d45896b0d6dc7392b0528db
Naveen-Dev022 <spnaveenshanmugam@outlook.com>
2026-09-02 23:57:07 +0530

    feat(repo): implement Member repository abstraction
    - Added Photo entoty
    -Added seeding data from json

--- API/Controllers/MembersController.cs
+++ API/Controllers/MembersController.cs
@@ -1,26 +1,32 @@
-using API.Data;
 using API.Entities;
+using API.Interfaces;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    public class MembersController(AppDbContext context) : BaseApiController
+    [Authorize]
+    public class MembersController(IMemberRepository memberRepository) : BaseApiController
     {
         [HttpGet]
-        public async Task<ActionResult<IReadOnlyList<AppUser>>> GetMembers()
+        public async Task<ActionResult<IReadOnlyList<Member>>> GetMembers()
         {
-            return await context.Users.ToListAsync<AppUser>();
+            return Ok(await memberRepository.GetMembersAsync());
         }
-        [Authorize]
         [HttpGet("{id}")]
-        public async Task<ActionResult<AppUser>> GetMembersById(string id)
+        public async Task<ActionResult<Member>> GetMembersById(string id)
         {
-            var member = await context.Users.FindAsync(id);
+            var member = await memberRepository.GetMemberByIdAsync(id);
             if (member is null) return NotFound();
             return member;
+        }
+
+        [HttpGet("{id}/photos")]
+        public async Task<ActionResult<IReadOnlyList<Photo>>> GetPhotoForMembersById(string id)
+        {
+            var photos = await memberRepository.GetPhotoForMemberAsync(id);
+            if (photos is null) return NotFound();
+            return Ok(photos);
         }
     }
 }
--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,6 @@
 using System.Text;
 using API.Data;
+using API.Entities;
 using API.Interfaces;
 using API.Middleware;
 using API.Services;
@@ -25,6 +26,8 @@ builder.Services.AddDbContext<AppDbContext>(option =>
 
 builder.Services.AddCors();
 builder.Services.AddScoped<ITokenService, TokenService>();
+builder.Services.AddScoped<IMemberRepository, MemberRepository>();
+
 
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 .AddJwtBearer(options =>
@@ -56,5 +59,22 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+
+//SeedData
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+
+try
+{
+    var context = services.GetRequiredService<AppDbContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedUsers(context);
+}
+catch (Exception ex)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error occured during migration");
+}
+
 
 app.Run();
